3ceadc43ab7abaedb008583744007e5b4de46abf
mylxsw <mylxsw@aicode.cc>
2023-11-07 10:14:37 +0800

    remove everything about tobias, because apple deny the app due to the alipay library

--- lib/page/balance/payment.dart
+++ lib/page/balance/payment.dart
@@ -3,13 +3,11 @@ import 'dart:async';
 import 'package:askaide/bloc/payment_bloc.dart';
 import 'package:askaide/helper/helper.dart';
 import 'package:askaide/helper/logger.dart';
-import 'package:askaide/helper/platform.dart';
 import 'package:askaide/lang/lang.dart';
 import 'package:askaide/page/balance/price_block.dart';
 import 'package:askaide/page/component/background_container.dart';
 import 'package:askaide/page/component/chat/markdown.dart';
 import 'package:askaide/page/component/enhanced_button.dart';
-import 'package:askaide/page/component/item_selector_search.dart';
 import 'package:askaide/page/component/loading.dart';
 import 'package:askaide/page/component/dialog.dart';
 import 'package:askaide/page/component/theme/custom_size.dart';
@@ -22,8 +20,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:go_router/go_router.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
-import 'package:tobias/tobias.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 class PaymentScreen extends StatefulWidget {
   final SettingRepository setting;
@@ -39,18 +35,14 @@ class _PaymentScreenState extends State<PaymentScreen> {
 
   @override
   void initState() {
-    if (PlatformTool.isIOS()) {
-      final purchaseUpdated = InAppPurchase.instance.purchaseStream;
-      _subscription = purchaseUpdated.listen((purchaseDetailsList) {
-        _listenToPurchaseUpdated(purchaseDetailsList);
-      }, onDone: () {
-        _subscription?.cancel();
-      }, onError: (error) {
-        showErrorMessage(resolveError(context, error));
-      });
-    } else {
-      // 支付宝支付
-    }
+    final purchaseUpdated = InAppPurchase.instance.purchaseStream;
+    _subscription = purchaseUpdated.listen((purchaseDetailsList) {
+      _listenToPurchaseUpdated(purchaseDetailsList);
+    }, onDone: () {
+      _subscription?.cancel();
+    }, onError: (error) {
+      showErrorMessage(resolveError(context, error));
+    });
 
     // 加载支付产品列表
     context.read<PaymentBloc>().add(PaymentLoadAppleProducts());
@@ -194,24 +186,6 @@ class _PaymentScreenState extends State<PaymentScreen> {
               textScaleFactor: 0.9,
             ),
           ),
-          // TextButton(
-          //   style: ButtonStyle(
-          //     overlayColor: MaterialStateProperty.all(Colors.transparent),
-          //   ),
-          //   onPressed: () {
-          //     _startPaymentLoading();
-          //     // 恢复购买
-          //     InAppPurchase.instance.restorePurchases().whenComplete(() {
-          //       _closePaymentLoading();
-          //       showSuccessMessage('恢复完成');
-          //     });
-          //   },
-          //   child: Text(
-          //     '恢复购买',
-          //     style: TextStyle(color: customColors.weakLinkColor),
-          //     textScaleFactor: 0.9,
-          //   ),
-          // ),
         ],
       ),
       backgroundColor: customColors.backgroundContainerColor,
@@ -312,53 +286,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
                             return;
                           }
 
-                          if (PlatformTool.isIOS() ||
-                              PlatformTool.isAndroid() ||
-                              PlatformTool.isMacOS()) {
-                            _startPaymentLoading();
-                            try {
-                              if (PlatformTool.isAndroid()) {
-                                await createAppAlipay();
-                              } else if (PlatformTool.isIOS()) {
-                                await createAppApplePay();
-                              } else {
-                                await createWebOrWapAlipay(source: 'web');
-                              }
-                            } catch (e) {
-                              _closePaymentLoading();
-                              showErrorMessage(resolveError(context, e));
-                            }
-                          } else {
-                            // openConfirmDialog(
-                            //   context,
-                            //   '当前终端在线支付暂不可用，预计最晚 2023 年 10 月 15 日恢复，如需充值，请使用移动端 APP（支持 Android 手机、Apple 手机）。',
-                            //   () {
-                            //     launchUrlString(
-                            //       'https://aidea.aicode.cc',
-                            //       mode: LaunchMode.externalApplication,
-                            //     );
-                            //   },
-                            //   confirmText: '前往下载移动端 APP',
-                            // );
-                            openListSelectDialog(
-                              context,
-                              <SelectorItem>[
-                                SelectorItem(const Text('支付宝电脑端（扫码支付）'), 'web'),
-                                SelectorItem(const Text('支付宝手机端'), 'wap'),
-                              ],
-                              (value) {
-                                _startPaymentLoading();
-                                createWebOrWapAlipay(source: value.value)
-                                    .onError((error, stackTrace) {
-                                  _closePaymentLoading();
-                                  showErrorMessageEnhanced(context, error!);
-                                });
-
-                                return true;
-                              },
-                              title: '请选择支付方式',
-                              heightFactor: 0.3,
-                            );
+                          _startPaymentLoading();
+                          try {
+                            await createAppApplePay();
+                          } catch (e) {
+                            _closePaymentLoading();
+                            // ignore: use_build_context_synchronously
+                            showErrorMessage(resolveError(context, e));
                           }
                         },
                       ),
@@ -409,108 +343,5 @@ class _PaymentScreenState extends State<PaymentScreen> {
     InAppPurchase.instance.buyConsumable(
       purchaseParam: PurchaseParam(productDetails: selectedProduct!),
     );
-  }
-
-  /// 创建支付宝付款（Web 或 Wap）
-  Future<void> createWebOrWapAlipay({required String source}) async {
-    final created = await APIServer().createAlipay(
-      selectedProduct!.id,
-      source: source,
-    );
-    paymentId = created.paymentId;
-
-    // 调起支付宝支付
-    launchUrlString(created.params).then((value) {
-      _closePaymentLoading();
-      openConfirmDialog(
-        context,
-        '请确认支付宝支付是否已完成',
-        () async {
-          _startPaymentLoading();
-          try {
-            final resp =
-                await APIServer().queryPaymentStatus(created.paymentId);
-            if (resp.success) {
-              showSuccessMessage(resp.note ?? '支付成功');
-              _closePaymentLoading();
-            } else {
-              // 支付失败，延迟 5s 再次查询支付状态
-              await Future.delayed(const Duration(seconds: 5), () async {
-                try {
-                  final value =
-                      await APIServer().queryPaymentStatus(created.paymentId);
-
-                  if (value.success) {
-                    showSuccessMessage(value.note ?? '支付成功');
-                  } else {
-                    showErrorMessage('支付未完成，我们接收到的状态为：${value.note}');
-                  }
-                  _closePaymentLoading();
-                } catch (e) {
-                  _closePaymentLoading();
-                  showErrorMessage(resolveError(context, e));
-                }
-              });
-            }
-          } catch (e) {
-            _closePaymentLoading();
-            showErrorMessage(resolveError(context, e));
-          }
-        },
-        confirmText: '已完成支付',
-        cancelText: '支付遇到问题，稍后继续',
-      );
-    });
-  }
-
-  /// 创建支付宝付款（App）
-  Future<void> createAppAlipay() async {
-    // 支付宝支付
-    final created = await APIServer().createAlipay(
-      selectedProduct!.id,
-      source: 'app',
-    );
-    paymentId = created.paymentId;
-
-    // 沙箱环境支持
-    final env = created.sandbox ? AliPayEvn.SANDBOX : AliPayEvn.ONLINE;
-
-    // 调起支付宝支付
-    final aliPayRes = await aliPay(
-      created.params,
-      evn: env,
-    ).whenComplete(() => _closePaymentLoading());
-    print("=================");
-    print(aliPayRes);
-    print(aliPayRes["resultStatus"]);
-    if (aliPayRes['resultStatus'] == '9000') {
-      await APIServer().alipayClientConfirm(
-        aliPayRes.map((key, value) => MapEntry(key.toString(), value)),
-      );
-
-      showSuccessMessage('购买成功');
-    } else {
-      switch (aliPayRes['resultStatus']) {
-        case 8000: // fall through
-        case 6004:
-          showErrorMessage('支付处理中，请稍后查看购买历史确认结果');
-          break;
-        case 4000:
-          showErrorMessage('支付失败');
-          break;
-        case 5000:
-          showErrorMessage('重复请求');
-          break;
-        case 6001:
-          showErrorMessage('支付已取消');
-          break;
-        case 6002:
-          showErrorMessage('网络连接出错');
-          break;
-        default:
-          showErrorMessage('支付失败');
-      }
-    }
-    print("-----------------");
   }
 }
