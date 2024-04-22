56535d12b62197f3cf3f18a60136068b44468467
zdb <zhoudongbin.zdb@alibaba-inc.com>
2024-04-22 16:19:31 +0800

    设置 右边滑入动画

--- lib/page/component/transition_resolver.dart
+++ lib/page/component/transition_resolver.dart
@@ -7,11 +7,14 @@ Page<dynamic> transitionResolver(Widget child, {bool useTransition = true}) {
     return CustomTransitionPage(
       child: child,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return FadeThroughTransition(
-          animation: animation,
-          secondaryAnimation: secondaryAnimation,
-          child: child,
-        );
+        const begin = Offset(1.0, 0.0); // 从右侧开始
+        const end = Offset.zero;
+        const curve = Curves.ease;
+
+        var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+        final offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(position: offsetAnimation, child: child);
       },
     );
   }
