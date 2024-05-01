d8a1a786432198eac36816e2818737f9ee013990
muhang <zhoudongbin.zdb@alibaba-inc.com>
2024-05-01 15:40:47 +0800

    添加 ai 换装

--- lib/page/creative_island/draw/components/image_selector_crop.dart
+++ lib/page/creative_island/draw/components/image_selector_crop.dart
@@ -1,11 +1,8 @@
 import 'dart:typed_data';
 
-import 'package:askaide/helper/haptic_feedback.dart';
-import 'package:askaide/helper/platform.dart';
 import 'package:askaide/lang/lang.dart';
 import 'package:askaide/page/component/image.dart';
 import 'package:askaide/page/component/theme/custom_theme.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localization/flutter_localization.dart';
 import 'package:image_picker/image_picker.dart';
@@ -59,120 +56,170 @@ class ImageSelectorCrop extends StatelessWidget {
         Material(
           borderRadius: BorderRadius.circular(8),
           color: customColors.backgroundColor,
-          child: InkWell(
-            borderRadius: BorderRadius.circular(8),
-            onTap: () async {
-              //ImageSource.camera 照相机 或 ImageSource.gallery 相册
-              ImagePickerHelper(context).pickWithCropImage(ImageSource.gallery,
-                  (croppedFile) {
-                onImageSelected(path: croppedFile.path);
-                //获取到剪切的文件路径，进行相关的操作
-                debugPrint("croppedFile:${croppedFile.path}");
-              });
-              // HapticFeedbackHelper.mediumImpact();
-              // FilePickerResult? result =
-              //     await FilePicker.platform.pickFiles(type: FileType.image);
-              // if (result != null && result.files.isNotEmpty) {
-              //   if (PlatformTool.isWeb()) {
-              //     onImageSelected(data: result.files.first.bytes!);
-              //   } else {
-              //     onImageSelected(path: result.files.first.path!);
-              //   }
-              // }
-            },
-            child: Container(
-                decoration: BoxDecoration(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                InkWell(
                   borderRadius: BorderRadius.circular(8),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  child: Row(
-                    children: [
-                      Stack(
-                        children: [
-                          Container(
-                            decoration: (selectedImagePath != null &&
-                                        selectedImagePath!.isNotEmpty) ||
-                                    (selectedImageData != null &&
-                                        selectedImageData!.isNotEmpty)
-                                ? BoxDecoration(
-                                    image: DecorationImage(
-                                      image: (selectedImagePath != null
-                                          ? resolveImageProvider(selectedImagePath!)
-                                          : (selectedImageData != null
-                                              ? MemoryImage(selectedImageData!)
-                                              : null))!,
-                                      fit: BoxFit.cover,
-                                    ),
-                                    color: customColors.backgroundContainerColor
-                                        ?.withAlpha(100),
-                                    borderRadius: BorderRadius.circular(8),
-                                  )
-                                : null,
-                            child: SizedBox(
-                              width: 160,
-                              height: height ?? 200,
-                            ),
-                          ),
-                          selectedImagePath == null || selectedImagePath!.isEmpty
-                              ? SizedBox(
-                            width: 160,
-                                  height: height ?? 200,
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(
-                                        Icons.camera_alt,
-                                        size: 30,
-                                        color: customColors.chatInputPanelText,
-                                      ),
-                                      const SizedBox(width: 10),
-                                      Text(
-                                        AppLocale.selectImage.getString(context),
-                                        style: TextStyle(
-                                          fontSize: 14,
-                                          fontWeight: FontWeight.w500,
-                                          color: customColors.chatInputPanelText
-                                              ?.withOpacity(0.8),
-                                        ),
-                                      ),
-                                    ],
+                  onTap: () async {
+                    //ImageSource.camera 照相机 或 ImageSource.gallery 相册
+                    ImagePickerHelper(context)
+                        .pickWithCropImage(ImageSource.gallery, (croppedFile) {
+                      onImageSelected(path: croppedFile.path);
+                      //获取到剪切的文件路径，进行相关的操作
+                      debugPrint("croppedFile:${croppedFile.path}");
+                    });
+                    // HapticFeedbackHelper.mediumImpact();
+                    // FilePickerResult? result =
+                    //     await FilePicker.platform.pickFiles(type: FileType.image);
+                    // if (result != null && result.files.isNotEmpty) {
+                    //   if (PlatformTool.isWeb()) {
+                    //     onImageSelected(data: result.files.first.bytes!);
+                    //   } else {
+                    //     onImageSelected(path: result.files.first.path!);
+                    //   }
+                    // }
+                  },
+                  child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Row(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  decoration: (selectedImagePath != null &&
+                                              selectedImagePath!.isNotEmpty) ||
+                                          (selectedImageData != null &&
+                                              selectedImageData!.isNotEmpty)
+                                      ? BoxDecoration(
+                                          image: DecorationImage(
+                                            image: (selectedImagePath != null
+                                                ? resolveImageProvider(
+                                                    selectedImagePath!)
+                                                : (selectedImageData != null
+                                                    ? MemoryImage(
+                                                        selectedImageData!)
+                                                    : null))!,
+                                            fit: BoxFit.cover,
+                                          ),
+                                          color: customColors
+                                              .backgroundContainerColor
+                                              ?.withAlpha(100),
+                                          borderRadius:
+                                              BorderRadius.circular(8),
+                                        )
+                                      : null,
+                                  child: SizedBox(
+                                    width: 160,
+                                    height: height ?? 200,
                                   ),
-                                )
-                              : Positioned(
-                                  bottom: 0,
-                                  left: 0,
-                                  right: 0,
-                                  child: Container(
-                                    color: const Color.fromARGB(80, 255, 255, 255),
-                                    height: 50,
-                                    child: const Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        Icon(
-                                          Icons.camera_alt,
-                                          size: 30,
-                                          color: Color.fromARGB(147, 255, 255, 255),
+                                ),
+                                selectedImagePath == null ||
+                                        selectedImagePath!.isEmpty
+                                    ? SizedBox(
+                                        width: 160,
+                                        height: height ?? 200,
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            Icon(
+                                              Icons.camera_alt,
+                                              size: 30,
+                                              color: customColors
+                                                  .chatInputPanelText,
+                                            ),
+                                            const SizedBox(width: 10),
+                                            Text(
+                                              AppLocale.selectImage
+                                                  .getString(context),
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500,
+                                                color: customColors
+                                                    .chatInputPanelText
+                                                    ?.withOpacity(0.8),
+                                              ),
+                                            ),
+                                          ],
                                         ),
-                                        SizedBox(width: 10),
-                                        Text(
-                                          '点击此处更换图片',
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            fontWeight: FontWeight.w500,
-                                            color:
-                                                Color.fromARGB(147, 255, 255, 255),
+                                      )
+                                    : Positioned(
+                                        bottom: 0,
+                                        left: 0,
+                                        right: 0,
+                                        child: Container(
+                                          color: const Color.fromARGB(
+                                              80, 255, 255, 255),
+                                          height: 50,
+                                          child: const Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              Icon(
+                                                Icons.camera_alt,
+                                                size: 30,
+                                                color: Color.fromARGB(
+                                                    147, 255, 255, 255),
+                                              ),
+                                              SizedBox(width: 10),
+                                              Text(
+                                                '点击此处更换图片',
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: Color.fromARGB(
+                                                      147, 255, 255, 255),
+                                                ),
+                                              ),
+                                            ],
                                           ),
                                         ),
-                                      ],
-                                    ),
-                                  ),
-                                )
-                        ],
-                      ),
-                      SizedBox(child: Text("data"),)],
+                                      )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+                Container(
+                  width: 200,
+                  height: height ?? 200,
+                  child: Text("data"),
+                  // height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+
                   ),
-                )),
+                ),
+                Container(
+                  height: height ?? 200,
+                  child: Text("data"),
+                  // height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+
+                  ),
+                ),
+                Container(
+                  height: height ?? 200,
+                  child: Text("data"),
+                  // height: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(8),
+
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ],
