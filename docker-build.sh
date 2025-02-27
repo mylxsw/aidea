247f3376b1628c1584966256981a8b0b331a649b
管宜尧 <yiyao.guan@felixsphere.com>
2025-02-27 22:06:28 +0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,5 @@ VERSION=2.0.0
 rm -fr build/web
 
 flutter build web --web-renderer canvaskit --release --dart-define=API_SERVER_URL=/
-docker buildx build --platform=linux/amd64,linux/arm64 -t mylxsw/aidea-web:$VERSION . --push
+docker buildx build --platform=linux/amd64 -t mylxsw/aidea-web:$VERSION . --push
 
